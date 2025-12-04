838848a3b7a347b3f537fd9205c77d1b6723150f
SafronovMaks1m <safranov01@list.ru>
2025-12-04 22:02:06 +0300

    check

---
+++ tasks/safronov_m_bubble_sort_odd_even/mpi/include/ops_mpi.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "safronov_m_bubble_sort_odd_even/common/include/common.hpp"
+#include "task/include/task.hpp"
+
+namespace safronov_m_bubble_sort_odd_even {
+
+class SafronovMBubbleSortOddEvenMPI : public BaseTask {
+ public:
+  static constexpr ppc::task::TypeOfTask GetStaticTypeOfTask() {
+    return ppc::task::TypeOfTask::kMPI;
+  }
+  explicit SafronovMBubbleSortOddEvenMPI(const InType &in);
+
+ private:
+  bool ValidationImpl() override;
+  bool PreProcessingImpl() override;
+  bool RunImpl() override;
+  bool PostProcessingImpl() override;
+  static std::vector<int> CalculatingInterval(int size_prcs, int rank, int size_arr);
+  void SendingVector(int rank);
+  void OddEvenBubble(std::vector<int> &own_data, int own_size, int begin, int phase);
+  void DataExchange(std::vector<int> &own_data, int rank, int neighbor);
+  void BasisSortingLocalArrays(std::vector<int> &own_data, std::vector<int> &interval, int size_arr, int rank,
+                               int size);
+  void SendingResult(int rank);
+};
+
+}  // namespace safronov_m_bubble_sort_odd_even
---
+++ tasks/safronov_m_bubble_sort_odd_even/seq/src/ops_seq.cpp
@@ -0,0 +1,58 @@
+#include "safronov_m_bubble_sort_odd_even/seq/include/ops_seq.hpp"
+
+#include <numeric>
+#include <vector>
+
+#include "safronov_m_bubble_sort_odd_even/common/include/common.hpp"
+#include "util/include/util.hpp"
+
+namespace safronov_m_bubble_sort_odd_even {
+
+SafronovMBubbleSortOddEvenSEQ::SafronovMBubbleSortOddEvenSEQ(const InType &in) {
+  SetTypeOfTask(GetStaticTypeOfTask());
+  GetInput() = in;
+}
+
+bool SafronovMBubbleSortOddEvenSEQ::ValidationImpl() {
+  return GetOutput().empty();
+}
+
+bool SafronovMBubbleSortOddEvenSEQ::PreProcessingImpl() {
+  GetOutput().clear();
+  return true;
+}
+
+bool SafronovMBubbleSortOddEvenSEQ::RunImpl() {
+  bool flag = true;
+  std::vector<int> array = GetInput();
+  if (array.size() == 0) {
+    return true;
+  }
+  while (flag) {
+    flag = false;
+    for (int j = 0; j < array.size() - 1; j += 2) {
+      if (array[j] > array[j + 1]) {
+        int tmp = array[j + 1];
+        array[j + 1] = array[j];
+        array[j] = tmp;
+        flag = true;
+      }
+    }
+    for (int j = 1; j < array.size() - 1; j += 2) {
+      if (array[j] > array[j + 1]) {
+        int tmp = array[j + 1];
+        array[j + 1] = array[j];
+        array[j] = tmp;
+        flag = true;
+      }
+    }
+  }
+  GetOutput() = array;
+  return true;
+}
+
+bool SafronovMBubbleSortOddEvenSEQ::PostProcessingImpl() {
+  return true;
+}
+
+}  // namespace safronov_m_bubble_sort_odd_even
